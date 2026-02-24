5cabd81b8814b9c475279892124ef91e63b0a746
Egor Zhulanov <egorzhulanovbar0313@gmail.com>
2026-02-24 22:55:20 +0500

    Wrote functions to draw regular geometric shapes

--- 01_shapes.py
+++ 01_shapes.py
@@ -29,6 +29,40 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+def triangle(point, angle, lenght):
+    for _ in range(3):
+        v = sd.get_vector(start_point=point, angle=angle, length=lenght)
+        v.draw()
+        angle += 120
+        point = v.end_point
+        
+def square(point, angle, lenght):
+    for _ in range(4):
+        v = sd.get_vector(start_point=point, angle=angle, length=lenght)
+        v.draw()
+        angle += 90
+        point = v.end_point
+
+def pentagon(point, angle, lenght):
+    for _ in range(5):
+        v = sd.get_vector(start_point=point, angle=angle, length=lenght)
+        v.draw()
+        angle += 72
+        point = v.end_point
+
+def hexagon(point, angle, lenght):
+    for _ in range(6):
+        v = sd.get_vector(start_point=point, angle=angle, length=lenght)
+        v.draw()
+        angle += 60
+        point = v.end_point
+    
+triangle(sd.get_point(100, 100), 20, 100)
+square(sd.get_point(400,100), 20, 100)
+pentagon(sd.get_point(100,400), 20, 100)
+hexagon(sd.get_point(400,370), 20, 100)
+
+
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
 # Скажем, связывать точки не линиями, а дугами. Или двойными линиями. Или рисовать круги в угловых точках. Или...
